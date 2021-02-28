c3825907d19278ab4e3d8af5be00c9a6f7817399
seiya-Unity <pazu70@gmail.com>
2021-02-28 16:37:20 +0900

    鉄球をランダムに落ちるように追加

---
+++ 自作ゲーム/自作ゲームプロジェクト/Assets/BallGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    float span = 1.0f;
+    float delta = 0;
+   
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(ballPrefab) as GameObject;
+            int px = Random.Range(-6, 7);
+            go.transform.position = new Vector3(px, 6, 0);
+        }
+    }
+}
+
