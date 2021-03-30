176ab2b4ceb6d7ee22169cebfbb99439ba1b4535
seiya-Unity <pazu70@gmail.com>
2021-03-30 21:10:51 +0900

    ゲームシーン完成

--- pick up ball/Assets/ItemGenerator.cs
+++ pick up ball/Assets/ItemGenerator.cs
@@ -9,10 +9,14 @@ public class ItemGenerator : MonoBehaviour
     float span = 1.0f;
     float delta = 0;
     int ratio = 5;
+    float speed = -0.03f;
+
     // Start is called before the first frame update
-    void Start()
+    public void SetParameter(float span,float speed,int ratio)
     {
-        
+        this.span = span;
+        this.speed = speed;
+        this.ratio = ratio;
     }
 
     // Update is called once per frame
@@ -34,6 +38,7 @@ public class ItemGenerator : MonoBehaviour
             }
             float x = Random.Range(-7,7);
             item.transform.position = new Vector2(x, 6);
+            item.GetComponent<ItemController>().dropSpeed = this.speed;
         }
     }
 }
