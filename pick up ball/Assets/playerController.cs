60b370c98677e2185d913984f2ddabdff73c67c0
seiya-Unity <pazu70@gmail.com>
2021-03-30 17:17:56 +0900

    当たり判定、playerコントロール

---
+++ pick up ball/Assets/playerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="apple")
+        {
+            Debug.Log("Tag=Apple");
+        }
+        else
+        {
+            Debug.Log("Tag=Bomb");
+        }
+        Destroy(other.gameObject);
+    }
+
+    // Update is called once per frame
+    public void LButtonDown()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+    public void RButtonDown()
+    {
+        transform.Translate(1, 0, 0);
+    }
+}
